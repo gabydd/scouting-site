c46e18c8b523789f6ca6b0d1bad8e019645b5e1e
gaby <gabydinnerdavid@gmail.com>
2021-12-18 11:07:33 -0500

    add new lines at end of files

---
+++ components/NumberInput.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const TextInput: FC = (props) => {
+  return <div></div>;
+};
+
+export default TextInput;
--- games/index.ts
+++ games/index.ts
@@ -1,6 +1,6 @@
 import INFINITE_RECHARGE from "./infinite-recharge";
 
 const GAMES = {
-    INFINITE_RECHARGE
-}
-export default GAMES
+  INFINITE_RECHARGE,
+};
+export default GAMES;
